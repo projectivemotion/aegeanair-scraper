a6ddd4b46172613423d4b6a98e8b936fd269f55d
Amado Martinez <amado@projectivemotion.com>
2017-01-27 14:47:14 -0600

    Added display prices as table option.

--- demo/aegean.php
+++ demo/aegean.php
@@ -20,7 +20,7 @@ foreach($autoload_files as $autoload_file)
 
 
 if($argc < 5)
-    die("Usage:\n\t$argv[0] origin destination outbound-date inbound-date\n" .
+    die("Usage:\n\t$argv[0] origin destination outbound-date inbound-date [--table]\n" .
         "Example:\n\t$argv[0] AMS ATH 2017-11-21 2017-11-25\n\n");
 
 
@@ -28,6 +28,7 @@ $origin = $argv[1];
 $destination = $argv[2];
 $departure_date_str = $argv[3];
 $return_date_str = $argv[4];
+$astable    =   isset($argv[5]);
 
 $Scraper    =   new \projectivemotion\AegeanScraper\Scraper();
 $Q = new \projectivemotion\AegeanScraper\Query();
@@ -37,6 +38,25 @@ $Q->setOrigin($origin);
 $Q->setDepartureDate(date_create_from_format('Y-m-d', $departure_date_str));
 $Q->setReturnDate(date_create_from_format('Y-m-d', $return_date_str));
 
+//$Q->setAdults(2);
+
 $flights = $Scraper->getFlights($Q);
 
-echo \json_encode($flights, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+if(!$astable)
+    echo \json_encode($flights, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+else{
+    printf("%-8s|%-8s|%8s|%8s|%8s|%-16s\n",
+        "from", "to", "flex", "business", "light", "flights"
+    );
+    foreach($flights as $flight){
+        printf("%-8s|%-8s|%8s|%8s|%8s|%-16s\n",
+        $flight->origin,
+            $flight->destination,
+            $flight->price->FLEXTEST ? $flight->price->FLEXTEST->amount . ' ' .  $flight->price->FLEXTEST->currency : '',
+            $flight->price->BUSINESTES ? $flight->price->BUSINESTES->amount. ' ' .  $flight->price->BUSINESTES->currency : '',
+            $flight->price->LIGHT ? $flight->price->LIGHT->amount. ' ' .  $flight->price->LIGHT->currency : '',
+            implode(', ', array_keys($flight->segments))
+        );
+    }
+
+}
